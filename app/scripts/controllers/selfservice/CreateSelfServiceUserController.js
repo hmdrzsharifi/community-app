444ac8e8eb04197ea56396dd7802c8b74424634c
hmdrzsharifi <hmdrzsharifi@gmail.com>
2020-05-22 17:52:23 +0430

    adding assign role to self service client page

--- app/scripts/controllers/selfservice/CreateSelfServiceUserController.js
+++ app/scripts/controllers/selfservice/CreateSelfServiceUserController.js
@@ -29,6 +29,51 @@
                 scope.formData.clients = [scope.clientId];
             });
 
+            scope.addRole = function () {
+                for (var i in this.available) {
+                    for (var j in scope.availableRoles) {
+                        if (scope.availableRoles[j].id == this.available[i]) {
+                            var temp = {};
+                            temp.id = this.available[i];
+                            temp.name = scope.availableRoles[j].name;
+                            scope.selectedRoles.push(temp);
+                            scope.availableRoles.splice(j, 1);
+                        }
+                    }
+                }
+                //We need to remove selected items outside of above loop. If we don't remove, we can see empty item appearing
+                //If we remove available items in above loop, all items will not be moved to selectedRoles
+                for (var i in this.available) {
+                    for (var j in scope.selectedRoles) {
+                        if (scope.selectedRoles[j].id == this.available[i]) {
+                            scope.available.splice(i, 1);
+                        }
+                    }
+                }
+            };
+            scope.removeRole = function () {
+                for (var i in this.selected) {
+                    for (var j in scope.selectedRoles) {
+                        if (scope.selectedRoles[j].id == this.selected[i]) {
+                            var temp = {};
+                            temp.id = this.selected[i];
+                            temp.name = scope.selectedRoles[j].name;
+                            scope.availableRoles.push(temp);
+                            scope.selectedRoles.splice(j, 1);
+                        }
+                    }
+                }
+                //We need to remove selected items outside of above loop. If we don't remove, we can see empty item appearing
+                //If we remove selected items in above loop, all items will not be moved to availableRoles
+                for (var i in this.selected) {
+                    for (var j in scope.availableRoles) {
+                        if (scope.availableRoles[j].id == this.selected[i]) {
+                            scope.selected.splice(i, 1);
+                        }
+                    }
+                }
+            };
+
             scope.submit = function(){
                 for (var i in scope.selectedRoles) {
                     scope.formData.roles.push(scope.selectedRoles[i].id) ;
@@ -40,7 +85,7 @@
                         controller: SuccessModalCtrl,
                     });
                 });
-                
+
             }
 
             var SuccessModalCtrl = function ($scope, $uibModalInstance) {
